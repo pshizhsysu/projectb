d9453e5e26c456df5765929bd3e4ac1e0fcf9434
pengsz <pengshzh@chinatelecom.cn>
2021-09-01 10:49:52 +0800

    first

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pshizhsysu/projectb
+
+go 1.16
+
+require github.com/pshizhsysu/projectc v1.0.0
---
+++ hello.go
@@ -0,0 +1,11 @@
+package projectb
+
+import (
+	"fmt"
+
+	"github.com/pshizhsysu/projectc"
+)
+
+func SayHelloWorld() {
+	fmt.Println("Hello " + projectc.World())
+}
